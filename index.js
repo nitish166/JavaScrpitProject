11f9a2d676b98eab90692652436150d8fbc6c372
Nitish kumar <knitish123@gmail.com>
2020-01-06 20:50:02 +0530

    Temporary Updated! Will update complete soon.

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
- prompt("What is your name");
- prompt("What is their name");
 
-var loveScore= Math.random();
-loveScore = loveScore*100;
-loveScore=Math.floor(loveScore);
-if(loveScore>=70)
-{
-    alert("Your love score is :"+loveScore+"% "+"You love is each other like tom and jerry");
+// for the left side dice images
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";  // dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var images1 = document.querySelectorAll("img")[0];
+
+images1.setAttribute("src", randomImageSource);
+
+
+
+// for the right side dice images
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var images2 = document.querySelectorAll("img")[1];
+
+images2.setAttribute("src", randomImageSource2);
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
-else if(loveScore>30 && loveScore<70)
+else if(randomNumber1<randomNumber2)
 {
-    alert("Your love score  is :"+loveScore+"%");
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
 }
-else
-{
-    alert("Your love score  is :"+loveScore+"%"+"You go together like oil and water");
+else{
+  document.querySelector("h1").innerHTML = "Match Draw!";
 }
